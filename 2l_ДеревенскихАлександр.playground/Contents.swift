1a01347866e830fb404e48e9ee8905934e7d5400
Derevenskikh Aleksander <hedgehog@kartis.ru>
2022-04-01 20:55:07 +0300

    try to release direct remove in 'for in array'

--- 2l_ДеревенскихАлександр.playground/Contents.swift
+++ 2l_ДеревенскихАлександр.playground/Contents.swift
@@ -33,21 +33,36 @@ func isNumberMulti3(_ value: Int) -> Bool {
 
 // 3. Создать возрастающий массив из 100 чисел.
 
-var ladder = [Int](repeating: 0, count: 100 )
+func makeLadderArray(_ count: Int) -> [Int] {
+    var ladder = [Int](repeating: 0, count: count )
+    for index in 0...ladder.count-1 {
+        ladder[index] = index
+    }
 
-for index in 0...ladder.count-1 {
-    ladder[index] = index
+    return ladder
 }
+
+
 
 // 4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
 
-var index = ladder.count - 1
+var ladderIndexed = makeLadderArray(100)
+
+var index = ladderIndexed.count - 1
 
 while index > 0 {
-    if isNumberEven(ladder[index]) || !isNumberMulti3(ladder[index]) {
-        ladder.remove(at: index)
+    if isNumberEven(ladderIndexed[index]) || !isNumberMulti3(ladderIndexed[index]) {
+        ladderIndexed.remove(at: index)
     }
     index -= 1
 }
+
+var ladderForIn = makeLadderArray(100)
+
+for item in ladderForIn where isNumberEven(item) || !isNumberMulti3(item) {
+    ladderForIn.removeFirst(item)
+}
+print (ladderIndexed)
+print (ladderForIn)
 
 
